7a4ccb1d557f931bb6d4c5ef7fce75c49b5fe1ca
friendship41 <stage41@nate.com>
2020-07-18 21:12:42 +0900

    Initial commit

---
+++ src/main/java/com/friendship41/vodserver/VodServerApplication.java
@@ -0,0 +1,13 @@
+package com.friendship41.vodserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VodServerApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(VodServerApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/friendship41/vodserver/VodServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.friendship41.vodserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VodServerApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
