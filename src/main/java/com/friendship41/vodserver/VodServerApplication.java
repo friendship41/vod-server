409f840c5f7a4e9560c7c047022e95f5adc3b23b
friendship41 <stage41@nate.com>
2020-07-21 23:38:56 +0900

    도커 설정 마무리

--- src/main/java/com/friendship41/vodserver/VodServerApplication.java
+++ src/main/java/com/friendship41/vodserver/VodServerApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class VodServerApplication {
-
   public static void main(String[] args) {
     SpringApplication.run(VodServerApplication.class, args);
   }
